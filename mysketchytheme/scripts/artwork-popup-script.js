7ff01b33811de4d49a49c38ef217a92a71f52eee
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2024-11-20 15:14:59 +0100

    refactor & fix: popup features script

--- mysketchytheme/scripts/artwork-popup-script.js
+++ mysketchytheme/scripts/artwork-popup-script.js
@@ -1,5 +1,5 @@
-// I - AU CHARGEMENT DE LA PAGE
 
+// I - ACTIONS AU CHARGEMENT DE LA PAGE
 window.onload = function() {
 
     // Les images visibles : 
@@ -16,62 +16,110 @@ window.onload = function() {
 
         // Au clic sur chaque image,
         artwork.addEventListener("click", (event)=>{
-            // Repérer la popup avec l'id correspondant (mentionné dans la classe 'opens-')
             let targetedPopupId = event.target.classList[0].split("opens-").pop();
-            let targetedPopup = findPopupById(targetedPopupId);
-
-            // Si une popup existe avec cet id : 
-            if(targetedPopup){
-                // Afficher la popup et cacher les autres
-                displayTargetedPopupAndHideOthers(targetedPopupId, popups);
-            }
-        })
+            findDisplayActivatePopup(targetedPopupId)
+        });
     }
 }
 
 // II - LES FONCTIONS
 
-// Vérifier si l'id de popup ciblée existe
-function findPopupById(targetedPopupId){
-    let targetedPopup = document.getElementById(targetedPopupId);
-    return targetedPopup;
-}
+// 1 - FONCTIONS GLOBALES
 
-// Activer la popup ciblée s'il y en a une et cacher les autres
-function displayTargetedPopupAndHideOthers(targetPopupId, popupsArray){
-
-    let foundTargetedPopup = false;
-
-    // Chercher la popup cible par son id
-    for(let popup of popupsArray){
-
-        // Si on trouve une correspondance
-        if(popup.id == targetPopupId){
-
-            // Rendre la popup ciblée visible
-            popup.classList.remove("hide");
-            // Activer la popup ciblée : 
-            // a - les flèches de navigation
-            activatePopupNavArrows(popup, popupsArray);
-            // b - le bouton de fermeture
-            activatePopupCloseButton(popup);
-            // c - la fermeture de la popup si on clique hors du contenu
-            activatePopupCloseWhenClickOutsideContent(popup);
-            // d - activer touchmove pour les versions tactiles
-            activatePopupNavigationWithTouchmoveEvents(popup, popupsArray);
-
-            foundTargetedPopup = true;
-
-        }else{
-            // Cacher les popups non-ciblées
-            popup.classList.add("hide");
-        }
+// A - Trouver, afficher et activer une popup
+function findDisplayActivatePopup(targetId){
+    // Trouver
+    let target = findTargetPopupById(targetId);
+    if(target){
+        // Afficher
+        target.classList.remove("hide");
+        // Activer
+        activatePopup(target);
+    }else{
+        // Si aucune popup ne correspond à l'id recherché, il ne se passe rien ou la popup affichée se ferme
+        return;
     }
-    // Si l'id donné en param mène bien à une popup : bloquer le body en arrière plan 
-    changeBackGroundScroll(foundTargetedPopup);
+}
+// A.1 - Chercher une popup par son id
+function findTargetPopupById(targetPopupId){
+    return document.getElementById(targetPopupId);
+}
+// A.2 - Activer les fonctionnalités d'une popup à son affichage
+function activatePopup(popup){
+
+    // 1) Les propriétés de la popup
+    // Éléments d'interfaces qui contiendront des eventListeners : 
+    popup.closeBtn = popup.getElementsByClassName("popup_close")[0];
+    popup.navArrows = popup.getElementsByClassName("popup_arrow");
+
+    // 2) Les méthodes
+    // Les onglets de fermeture étant situés dans la zone hors du contenu, event.stopPropagation(); évite de déclencher deux fois la fermeture de la popup
+    popup.handleClickOnCloseButton = (event)=> {event.stopPropagation();closeAndDeactivateCurrentPopup(popup)};
+    popup.handleClickOutsideContent = (event)=>  closePopupOnClickOutsideContentZone(event, popup);
+    popup.handleClickInNavigationArrows = (event)=> {event.stopPropagation(); navigateOnClickOnPopupNavArrows(event, popup);}
+    popup.handleKeyboardArrowEvents = (event) => navigateOnKeyboardArrows(event, popup);
+    popup.handleSwipeEvent = (event)=> swipe(event, popup);
+
+    // 3) Les event listeners
+    // - Fermer la popup au clic sur l'onglet de fermeture
+    popup.closeBtn.addEventListener("click", popup.handleClickOnCloseButton);
+
+    // - Fermer la popup au clic en dehors de la zone de contenu
+    popup.addEventListener("click", popup.handleClickOutsideContent);
+
+    // - Changer de popup active au clic sur les onglets-flèches de navigation
+    for(let navArrow of popup.navArrows){
+        navArrow.addEventListener("click", popup.handleClickInNavigationArrows);
+    }  
+
+    // - Navigation au clavier
+    addEventListener("keyup", popup.handleKeyboardArrowEvents);
+
+    // - Écrans tactiles : navigation au swipe
+    popup.addEventListener("touchstart", popup.handleSwipeEvent);
+
+    // - Empêcher le scroll à l'arrière plan
+    changeBackGroundScroll(true);
 }
 
-// Empêcher ou rétablir le scroll de l'arrière-plan
+// B - Fermer et désactiver la popup active
+function closeAndDeactivateCurrentPopup(popup){
+    // Cacher
+    popup.classList.add("hide");
+    // Désactiver
+    deactivatePopup(popup);
+
+}
+// B.1 - Désactiver les fonctionnalités de la popup à sa fermeture
+function deactivatePopup(popup){
+
+    // - Désactiver le bouton de fermeture
+    popup.closeBtn.removeEventListener("click", popup.handleClickOnCloseButton);
+    // - Désactiver la fermeture au clic en dehors de la zone de contenu
+    popup.removeEventListener("click",popup.handleClickOutsideContent);
+    // - Désactiver les onglets-flèches de navigation
+    for(let navArrow of popup.navArrows){
+        navArrow.removeEventListener("click", popup.handleClickInNavigationArrows);
+    }
+    // - Désactiver la navigation au clavier, avec les flèches directionnelles
+    removeEventListener("keyup", popup.handleKeyboardArrowEvents)
+    // - Écrans tactiles : désactiver la navigation au swipe
+    popup.removeEventListener("touchstart", popup.handleSwipeEvent);
+
+    // - Rétablir le scroll à l'arrière plan
+    changeBackGroundScroll(false);
+}
+
+// C - Changer de popup active
+function changeActivePopup(currentPopup, newPopupId){
+    // Cacher - désactiver la popup courante
+    closeAndDeactivateCurrentPopup(currentPopup)
+
+    // Trouver - afficher - activer la nouvelle popup
+    findDisplayActivatePopup(newPopupId)
+}
+
+// D - Empêcher ou rétablir le scroll de l'arrière-plan
 function changeBackGroundScroll(boolAPopupIsOpened){
 
     let body = document.body;
@@ -86,104 +134,75 @@ function changeBackGroundScroll(boolAPopupIsOpened){
     }
 }
 
-// Activer les flèches de navigation sur la popup active
-function activatePopupNavArrows(activePopup, popupsArray){
+// 2 - LES FONCTIONNALITÉS DE LA POPUP
 
-    let navArrows = activePopup.getElementsByClassName("popup_arrow");
-    //let popupNavArrows = Array.from(document.getElementsByClassName("popup_arrow"));
-    for(let navArrow of navArrows){
+// Fermer la popup au clic sur l'onglet X => on fait directement appel à closeAndDeactivateCurrentPopup 
 
-        // Repérer la popup suivante ou précédente avec l'id "goto-" sur la flèche
-        let targetedPopupId = navArrow.id.split("goto-").pop();
-
-        navArrow.addEventListener(("click"), ()=>{
-
-            //Afficher la popup ciblée et masquer les autres
-            displayTargetedPopupAndHideOthers(targetedPopupId, popupsArray);
-        });
+// Fermer la popup au clic en dehors de la zone de contenu
+function closePopupOnClickOutsideContentZone(clickEvent, popup){
+    let popupContent = popup.getElementsByClassName("popup_content")[0];
+    // Si la cible du clic ne se trouve pas dans la zone de contenu
+    if(!popupContent.contains(clickEvent.target)){
+        // Fermer la popup
+        closeAndDeactivateCurrentPopup(popup);
     }
-};
+}
 
-// Activer le bouton de fermeture sur la popup active
-function activatePopupCloseButton(activePopup){
-    let closePopupBtn = activePopup.getElementsByClassName("popup_close")[0];
-    closePopupBtn.addEventListener("click", ()=>{
-        activePopup.classList.add("hide");
-        changeBackGroundScroll(false);
-    });
-};
+// Naviguer d'une popup à l'autre au clic sur les onglets-flèches
+function navigateOnClickOnPopupNavArrows(clickEvent, currentPopup){
+    // Repérer la popup suivante ou précédente avec l'id "goto-" sur la flèche
+    let newPopupId = clickEvent.target.id.split("goto-").pop();
+    // Relancer la recherche avec le nouvel id
+    changeActivePopup(currentPopup, newPopupId);
+}
 
-// Activer la fermeture de la popup au clique en dehors du contenu
-function activatePopupCloseWhenClickOutsideContent(activePopup){
+// Naviguer d'une popup à l'autre au clavier, avec les flèches directionnelles
+function navigateOnKeyboardArrows(keyboardEvent, activePopup){
 
-    let popupContent = activePopup.getElementsByClassName("popup_content")[0];
-    
-    activePopup.addEventListener("click", (event)=>{
-        if(!popupContent.contains(event.target)){
-            activePopup.classList.add("hide");
-            changeBackGroundScroll(false);
-        }
-    })
-};
+    let activePopupIdNumber = parseInt(activePopup.id);
+    let nextPopupIdNumber = activePopupIdNumber;
 
-// Activer la navigation au touchmove pour les versions tactiles
-function activatePopupNavigationWithTouchmoveEvents(activePopup, popupsArray){
+    if(keyboardEvent.key == "ArrowRight"){        
+        nextPopupIdNumber = activePopupIdNumber +=1;
 
-    let startX = 0;
-    let startY = 0;
-    const swipeThreshold = 30; // Seuil en pixels pour détecter un mouvement significatif
-    
-    activePopup.addEventListener("touchstart", (event) => {
-        // Enregistrer les coordonnées de départ
-        startX = event.touches[0].clientX;
-        startY = event.touches[0].clientY;
-    });
-    
-    activePopup.addEventListener("touchend", (event) => {
+    }else if(keyboardEvent.key == "ArrowLeft"){
+        nextPopupIdNumber = activePopupIdNumber -=1;
+    }
+    changeActivePopup(activePopup, nextPopupIdNumber.toString());
+}
+
+// Sur écrans tactiles : naviguer d'une popup à l'autre au swipe
+function swipe(swipeEvent, currentPopup){
+
+    // Seuil en pixels pour détecter un mouvement significatif
+    const swipeThreshold = 30; 
+
+    // Enregistrer les coordonnées de départ
+    startX = swipeEvent.touches[0].clientX;
+    startY = swipeEvent.touches[0].clientY;
+
+    currentPopup.addEventListener("touchend", (event) => {
         // Calculer les coordonnées de fin
         const endX = event.changedTouches[0].clientX;
         const endY = event.changedTouches[0].clientY;
-    
+
         // Déterminer le déplacement
         const deltaX = endX - startX;
         const deltaY = endY - startY;
-    
+
+        let activePopupIdNumber = parseInt(currentPopup.id);
+        let nextPopupIdNumber = activePopupIdNumber;
+
         // Si le swipe est principalement horizontal
         if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > swipeThreshold) {
             // Si on swipe à droite
             if (deltaX > 0) {
-                swipe(activePopup, "previous", popupsArray);
+                nextPopupIdNumber = activePopupIdNumber -=1;
             // Si on swipe à gauche
             } else {
-                swipe(activePopup, "next", popupsArray);
+                nextPopupIdNumber = activePopupIdNumber +=1;
             }
         }
-        
-        // On fait quelque chose pour un swipe vertical ?
-        /*
-        else if (Math.abs(deltaY) > swipeThreshold) {
-            if (deltaY > 0) {
-                console.log("Swipe bas détecté");
-            } else {
-                console.log("Swipe haut détecté");
-            }
-        }     
-        */
+        changeActivePopup(currentPopup, nextPopupIdNumber.toString());
     });
-}
-
-// Décider de la prochaine popup au swipe
-function swipe(activePopup, direction, popupsArray){
-    
-    let activePopupIdNumber = parseInt(activePopup.id);
-    let nextPopupIdNumber = activePopupIdNumber;
-
-    
-    if(direction == "previous"){
-        nextPopupIdNumber = activePopupIdNumber -=1;
-    }else if(direction == "next"){
-        nextPopupIdNumber = activePopupIdNumber +=1;
-    }
-    
-    displayTargetedPopupAndHideOthers(nextPopupIdNumber, popupsArray);
 }
