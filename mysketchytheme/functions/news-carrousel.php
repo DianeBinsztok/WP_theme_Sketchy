c9003c20d574164ea46ea0a49b715a9fe117c654
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-03-27 18:55:20 +0100

    feat: display post categories in slider

--- mysketchytheme/functions/news-carrousel.php
+++ mysketchytheme/functions/news-carrousel.php
@@ -23,12 +23,23 @@ function display_latest_news_in_slider()
 
         while ($query->have_posts()):
             $query->the_post();
+            $post_id = get_the_id();
+
 
             //slide
             echo ('<div class="slide" style="background-image:url(' . get_the_post_thumbnail_url() . ')">');
             echo '<div id="post-infos">';
             echo the_title('<h3>', '</h3>');
             echo the_excerpt();
+
+            echo '<div id="post-categories" style="display:flex;">';
+            $post_categories = get_the_category($post_id);
+            foreach ($post_categories as $category) {
+                echo '<p style="border:1px solid black; padding:0.3rem 0.7rem; margin:0.5rem;">' . $category->name . '</p>';
+
+            }
+            echo '</div>';
+
             echo '</div>';
             echo ('</div>');
         endwhile;
