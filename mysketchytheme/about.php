4689e964229dde8e68971a33ec8a1982ecb42ae8
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-05-02 17:28:32 +0200

    style: about.php basic styling

--- mysketchytheme/about.php
+++ mysketchytheme/about.php
@@ -6,10 +6,20 @@ get_header();
 ?>
 
 <section class="with-padding">
-    <h1>C'est parti, je raconte ma vie !</h1>
-    <?php
-    the_content();
-    ?>
+    <section id="about-head">
+        <?php
+        the_post_thumbnail();
+        ?>
+        <h1>C'est parti, je raconte ma vie !</h1>
+    </section>
+
+    <section id="about-content">
+        <?php
+        the_content();
+        ?>
+    </section>
+
+
 </section>
 <?php
 get_footer();
