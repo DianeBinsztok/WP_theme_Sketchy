04a59b9161f84318f64a58bc928041e79ac9717a
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-05-07 10:30:25 +0200

    style: front-page's gallery section

--- mysketchytheme/front-page.php
+++ mysketchytheme/front-page.php
@@ -12,7 +12,7 @@ get_header();
 </section>
 
 <section id="latest-news" class="with-padding">
-
+    <h2>Quoi de neuf?</h2>
     <?php
     display_latest_news_in_slider();
     ?>
@@ -20,40 +20,47 @@ get_header();
 </section>
 
 <section id="galleries-introduction" class="with-padding">
-    <div class="gallery-link_container">
-        <a href="<?php echo get_site_url(); ?>/artworks/?categorie=nu">
-            <div class="gallery-link"
-                style="background-image: url('<?php echo get_site_url(); ?>/wp-content/uploads/2024/10/47.jpg');">
-                <div class="gallery-link_text">
-                    <h2>Des gens tous nus</h2>
-                    <p>Je paie des sous pour aller dans des ateliers privés voir des gens tous nus.</p>
+    <h2>Visitez la galerie</h2>
+    <div class="gallery-links">
+        <div class="gallery-link_container">
+            <a href="<?php echo get_site_url(); ?>/artworks/?categorie=nu">
+                <div class="gallery-link"
+                    style="background-image: url('<?php echo get_site_url(); ?>/wp-content/uploads/2024/10/47.jpg');">
+                    <div class="gallery-link_text">
+                        <h3 class="gallery-link_title">Des gens tous nus</h3>
+                        <p class="gallery-link_description">Je paie des sous pour aller dans des ateliers privés voir
+                            des
+                            gens tous nus.</p>
+                    </div>
                 </div>
-            </div>
-        </a>
-    </div>
+            </a>
+        </div>
 
-    <div class="gallery-link_container">
-        <a href="<?php echo get_site_url(); ?>/artworks/?categorie=croquis">
-            <div class="gallery-link"
-                style="background-image: url('<?php echo get_site_url(); ?>/wp-content/uploads/2024/10/56.jpg');">
+        <div class="gallery-link_container">
+            <a href="<?php echo get_site_url(); ?>/artworks/?categorie=croquis">
+                <div class="gallery-link"
+                    style="background-image: url('<?php echo get_site_url(); ?>/wp-content/uploads/2024/10/56.jpg');">
 
-                <div class="gallery-link_text">
-                    <h2>Des croquis</h2>
-                    <p>Quand je m'ennuie, je croque des trucs... Je m'ennuie souvent et partout.</p>
+                    <div class="gallery-link_text">
+                        <h3 class="gallery-link_title">Des croquis</h3>
+                        <p class="gallery-link_description">Quand je m'ennuie, je croque des trucs... Je m'ennuie
+                            souvent et
+                            partout.</p>
+                    </div>
                 </div>
-            </div>
-        </a>
-    </div>
-    <div class="gallery-link_container">
-        <a href="<?php echo get_site_url(); ?>/artworks/?categorie=peinture">
-            <div class="gallery-link" style="
+            </a>
+        </div>
+        <div class="gallery-link_container">
+            <a href="<?php echo get_site_url(); ?>/artworks/?categorie=peinture">
+                <div class="gallery-link" style="
                 background-image: url('<?php echo get_site_url(); ?>/wp-content/uploads/2024/10/116-scaled.jpg');">
-                <div class="gallery-link_text">
-                    <h2>De l'huile</h2>
-                    <p>Mes rares tentatives.</p>
+                    <div class="gallery-link_text">
+                        <h3 class="gallery-link_title">De l'huile</h3>
+                        <p class="gallery-link_description">Mes rares tentatives.</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     </div>
 </section>
 <?php
