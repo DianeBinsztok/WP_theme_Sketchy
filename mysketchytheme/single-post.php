6ecbfe5a96aff2cb98f47228ce0c701c273a5f11
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2025-05-07 08:45:03 +0200

    fix: single-post: display square and large image on the right devices

--- mysketchytheme/single-post.php
+++ mysketchytheme/single-post.php
@@ -3,42 +3,36 @@ $title = get_the_title();
 $excerpt = has_excerpt() ? get_the_excerpt() : '';
 $date = get_the_date("j F, Y");
 
-// Pour les versions mobile : aller chercher le thumbnail custom: une image croppée à la main au format carré
-// Si la vignette custom n'est pas définie, afficher le thumbnail défini par WP
-$thumbnail = get_the_post_thumbnail($post->ID);
-$post_custom_thumbnail_id = get_post_meta($post->ID, 'post_img_custom_large_id', true);
 
-if ($post_custom_thumbnail_id) {
-    $thumbnail = wp_get_attachment_image($post_custom_thumbnail_id, 'medium');
+// VERSION MOBILE : IMAGE CARRÉE
+// Récupérer le post-meta "post_img_custom_square_id" s'il existe
+$post_custom_img_square_id = get_post_meta($post->ID, "post_img_custom_square_id", true);
+
+// Voir si un custom_img_square est enregistré
+if ($post_custom_img_square_id) {
+    $post_custom_img_square = wp_get_attachment_image($post_custom_img_square_id, "medium");
+    // Si custom_img_square n'a pas été renseigné, utiliser le thumbnail par défaut     
+} else {
+    $post_custom_img_square = get_the_post_thumbnail($post->ID, "medium");
+}
+
+// VERSIONS LARGES : IMAGE LARGE
+$post_custom_img_large_id = get_post_meta($post->ID, "post_img_custom_large_id", true);
+
+// Voir si un custom_img_large est enregistré
+if ($post_custom_img_large_id) {
+    $post_custom_img_large_url = wp_get_attachment_image_url($post_custom_img_large_id, 'large');
+    // Si custom_img_large n'a pas été renseigné, utiliser la featured image par défaut  
+} else {
+    $post_custom_img_large_url = get_the_post_thumbnail_url($post_id, 'large');
 }
 
 get_header();
 ?>
-<section id="post-head_large" class="with-padding"
-    style="background-image: url(<?php echo wp_get_attachment_image_url($post_custom_thumbnail_id, "large") ?>);">
-    <div id="title-excerpt-date_container">
-        <h1 id="title">
-            <?php
-            echo $title;
-            ?>
-        </h1>
-        <div id="excerpt">
-            <?php
-            echo $excerpt;
-            ?>
-        </div>
-
-        <div id="date" style="font-style:italic">
-            <?php
-            echo $date;
-            ?>
-        </div>
-    </div>
-</section>
-
+<!-- EN-TÊTE MOBILE -->
 <section id="post-head_mobile" class="with-padding">
     <div id="post-head_img">
-        <?php echo $thumbnail ?>
+        <?php echo $post_custom_img_square ?>
     </div>
 
     <section id="post-head_info_container" class="with-padding">
@@ -60,6 +54,29 @@ get_header();
             </div>
         </div>
     </section>
+</section>
+
+<!-- EN-TÊTE TABLETTES ET DESKTOP -->
+<section id="post-head_large" class="with-padding"
+    style="background-image: url(<?php echo $post_custom_img_large_url ?>);">
+    <div id="title-excerpt-date_container">
+        <h1 id="title">
+            <?php
+            echo $title;
+            ?>
+        </h1>
+        <div id="excerpt">
+            <?php
+            echo $excerpt;
+            ?>
+        </div>
+
+        <div id="date" style="font-style:italic">
+            <?php
+            echo $date;
+            ?>
+        </div>
+    </div>
 </section>
 
 <section class="with-padding">
